2bfc76f14d40359d214f61389ceab3382f7059a5
NANTHINIRAJENDRAN <nanthiniraje@gmail.com>
2022-05-19 16:08:02 +0530

    siginnn

--- frontend/src/screens/Signin.js
+++ frontend/src/screens/Signin.js
@@ -6,7 +6,7 @@ const Signin = () => {
     <div className='row justify-content-center mx-5'>
        <div className='col-md-6 col-md-offset-3 justify-content-center'>
       <Form>
-        <Form.Label className='h1'> SIGN IN </Form.Label>
+        <Form.Label className='h1'> SIGN INnn </Form.Label>
         <Form.Group className="mb-3 mt-4" controlId="formGroupEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" className='w-75' placeholder="Enter email" />
